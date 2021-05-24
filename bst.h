26fcfbd323cd24207828fdd163b1bdf8076e03c1
Jose Terrones <66368520+RadeonXYZ@users.noreply.github.com>
2021-05-23 19:12:44 -0500

    bst.h modificado
    by jose terrones

--- bst.h
+++ bst.h
@@ -1,9 +1,10 @@
-#ifndef __BST_H__ //Maizo Diego
-#define __BST_H__  //kevin de lama
-
-#include <iostream> //Maizo
-#include <cassert>  // de Lama
-#include "type.h" //Maizo
+#ifndef __BST_H__ 
+#define __BST_H__  
+//by jose terrones
+#include <iostream> 
+#include <cassert> 
+#include <vector>
+#include "type.h"
 
 using namespace std;
 
@@ -12,30 +13,33 @@ class CBinaryTree
 {
     struct Node
     {
-      T data;//Maizo
+      T data;
       Node *m_pChild[2];
       Node *m_pParentNode;
-      Node(const T &_data, Node *pParentNode, Node *pLeft=nullptr, Node *pRight=nullptr) //Maizo agregado const
+      Node(const T &_data, Node *pParentNode, Node *pLeft=nullptr, Node *pRight=nullptr)
         : data(_data)
       {   m_pChild[0] = pLeft;  m_pChild[1] = pRight;  m_pParentNode = pParentNode; }
-      T getData()             { return data; } //Maizo
-      void setData (T &_data) { data=_data; }  //Maizo
+      T getData()             { return data; } 
+      void setData (T &_data) { data=_data; } 
       Node *getParent()       { return m_pParentNode; }
     };
-public:
+public: //Iterador
     class Iterator
     {
           Node *m_pNode;
-          const Node *getNode() { return m_pNode;  }
-          void setNode(const Node *pNode) { m_pNode = pNode;  }
+          Node *getNode() { return m_pNode;  }
+          void setNode(Node *pNode) { m_pNode = pNode;  }
       public:    
           Iterator(Node *pNode=nullptr): m_pNode(pNode) {};
           Iterator(Iterator &other) : m_pNode(other.getNode())  {}
           T operator *() { assert(getNode() != nullptr); return getNode()->getData(); }
-          Iterator operator++();
+          Iterator operator++(); //funcion iterador InOrder
+          Iterator operator--(); //funcion iterador InOrder Inverso
           bool operator==(Iterator &other)  {return getNode() == other.getNode(); }
-          bool operator!=(Iterator &other)  {return !(*this == other); }
+          bool operator!=(Iterator &other)  {return getNode()!=other.getNode(); }
     };
+    //funciones optimizadas
+     //comienzo y fin de iterador InOrder
     Iterator begin()
     {
       Node *pNode = getRoot();
@@ -44,12 +48,22 @@ public:
       return Iterator(pNode);
     }
     Iterator end()  { return Iterator(nullptr); }
+    //comienzo y fin de iterador InOrder Inverso
+    Iterator newend()
+    {
+      Node *pNode = getRoot();
+      while(pNode->m_pChild[1])
+        pNode = pNode->m_pChild[1];
+      return Iterator(pNode);
+    }
+
+    Iterator newbegin()  { return Iterator(nullptr); }
 private:
     Node *m_pRoot = nullptr;
     Node *getRoot() { return m_pRoot; }
 public:
     CBinaryTree() {}
-    void insert_old(const T &dato) //Maizo agregado const
+    void insert_old(const T &dato)
     {   
         Node **ppNode = &m_pRoot;
         while(*ppNode != nullptr)      
@@ -60,27 +74,59 @@ public:
         }
         *ppNode = new Node(dato);
     }
-    void insert(const T &dato) //Maizo agregado const
+    void insert(const T &dato) 
     {   internal_insert(m_pRoot, nullptr, dato);   }
 
-    void Preorder()
+    void Preorder() //funcion PreOrden
     { PreOrden(m_pRoot, 0, 1, "", "",cout);  }
     
-    void Postorder()
+    void Postorder() //funcion PostOrden
     { PostOrden(m_pRoot, 1, 0, "", "",cout);  }
 
-    void Inorder()
+    void Inorder() //funcion Inorder
     { InOrden(m_pRoot, 1, 0, "", "",cout); }
+
+    void Preorder2d() //funcion PreOrden en 2d
+    { PreOrden2d(m_pRoot, 0, 1," ", cout);  }
+    
+    void Postorder2d() //funcion PostOrden en 2d
+    { PostOrden2d(m_pRoot, 0, 1, " ",cout);  }
+
+    void Inorder2d() //funcion InOrden en ed
+    { InOrden2d(m_pRoot, 0, 1, " ",cout); }
+
+    void Borrar(const T &dato) //funcion borrar
+    { Deleted(m_pRoot, 0, 1,  dato);}
+    //funciones optimizadas
+    void Iterator_InOrder() //funcion iterador InOrder
+    { CBinaryTree<T1>::Iterator iter = begin();
+      CBinaryTree<T1>::Iterator iter2 = end();
+        for(; iter != iter2; ++(iter)) 
+      cout << *iter << endl; 
+    }
+
+    void Iterator_InOrder_Inv() //funcion iterador InOrder Inverso
+    { CBinaryTree<T1>::Iterator iter = newend();
+      CBinaryTree<T1>::Iterator iter2 = newbegin();
+        for(; iter != iter2; --(iter)) 
+      cout << *iter << endl; 
+    }
     
 private:
-    void internal_insert(Node *&rParentPointer, Node *rParentNode, const T &dato); //Maizo agregado const
+    void internal_insert(Node *&rParentPointer, Node *rParentNode, const T &dato); 
     void InOrden(Node *rParent, size_t c1, size_t c2, string space, string flecha, ostream &os);
     void PreOrden(Node *rParent, size_t c1, size_t c2, string space, string flecha, ostream &os);
     void PostOrden(Node *rParent, size_t c1, size_t c2, string space, string flecha, ostream &os);
+    void InOrden2d(Node *rParent, size_t c1, size_t c2, string espacio,  ostream &os);
+    void PreOrden2d(Node *rParent, size_t c1, size_t c2, string espacio,  ostream &os);
+    void PostOrden2d(Node *rParent, size_t c1, size_t c2, string espacio,  ostream &os);
+    Node *ElMenor(Node *rParent, size_t c1, size_t c2);//parte de borrar
+    Node *Deleted(Node *rParent, size_t c1, size_t c2,const T &dato);//parte de borrar
+
 };
 
 template <typename T>
-void CBinaryTree<T>::internal_insert(Node *&rParentPointer, Node *rParentNode, const T &dato) // kevin de lama y Maizo agregado const
+void CBinaryTree<T>::internal_insert(Node *&rParentPointer, Node *rParentNode, const T &dato)
 {
   if(!rParentPointer)
   {   rParentPointer = new Node(dato, rParentNode);
@@ -116,6 +162,7 @@ void CBinaryTree<T>::PreOrden(Node *rParent, size_t c1, size_t c2, string space,
   }
 }
 
+
 template <typename T>
 void CBinaryTree<T>::PostOrden(Node *rParent, size_t c1, size_t c2, string space, string flecha, ostream &os)
 {
@@ -128,6 +175,40 @@ void CBinaryTree<T>::PostOrden(Node *rParent, size_t c1, size_t c2, string space
 }
 
 template <typename T>
+void CBinaryTree<T>::PreOrden2d(Node *rParent, size_t c1, size_t c2, string espacio, ostream &os)
+{
+  if(rParent)
+  {
+    os << rParent->getData()<< espacio;
+    PreOrden2d(rParent->m_pChild[c1], c1, c2, espacio,  os);
+    PreOrden2d(rParent->m_pChild[c2], c1, c2, espacio,  os);
+  }
+}
+
+template <typename T>
+void CBinaryTree<T>::InOrden2d(Node *rParent, size_t c1, size_t c2, string espacio, ostream &os)
+{
+  if(rParent)
+  {
+    InOrden2d(rParent->m_pChild[c1], c1, c2, espacio,  os);
+    os << rParent->getData()<< espacio;
+    InOrden2d(rParent->m_pChild[c2], c1, c2, espacio,  os);
+  }
+}
+
+template <typename T>
+void CBinaryTree<T>::PostOrden2d(Node *rParent, size_t c1, size_t c2, string espacio, ostream &os)
+{
+  if(rParent)
+  {
+    PostOrden2d(rParent->m_pChild[c1], c1, c2, espacio,  os);
+    PostOrden2d(rParent->m_pChild[c2], c1, c2, espacio,  os);
+    os << rParent->getData()<< espacio;
+  }
+}
+
+
+template <typename T>
 typename CBinaryTree<T>::Iterator CBinaryTree<T>::Iterator::operator++()
 {
     Node *pNode = getNode();
@@ -138,19 +219,95 @@ typename CBinaryTree<T>::Iterator CBinaryTree<T>::Iterator::operator++()
         pNode = pNode->m_pChild[0];
       setNode(pNode);
       return *this;
-    }
-    Node *pParent = pNode->getParent();
-    while(pParent && pParent->m_pChild[1] == pNode) // Mientras venga por el lado derecho => subir
-    {
-      pNode = pParent;
-      pParent = pParent->getParent();
-    }
-    if(pParent && pParent->m_pChild[0] == pNode) // Soy el hijo izquierdo
-    { setNode(pParent);
-      return *this;
-    }
-    setNode(nullptr);
-    return *this;
+    } else {
+      Node *pParent = pNode->getParent();
+      while(pParent && pParent->m_pChild[1] == pNode) // Mientras venga por el lado derecho => subir
+      { pNode = pParent;
+        pParent = pParent->getParent();    }
+      if(pParent && pParent->m_pChild[0] == pNode) // Soy el hijo izquierdo
+      { setNode(pParent);
+      return *this; }
+     setNode(nullptr);
+    return *this;}
 }
 
-#endif //Maizo Diego  
+//lado opuesto del operador ++, si era 1 ahora es 0 en m_pChild
+template <typename T>
+typename CBinaryTree<T>::Iterator CBinaryTree<T>::Iterator::operator--()
+{
+    Node *pNode = getNode();
+    if(pNode->m_pChild[0]) // Tiene hijo izquierdo?
+    {
+      pNode = pNode->m_pChild[0];
+      while(pNode->m_pChild[1])
+        pNode = pNode->m_pChild[1];
+      setNode(pNode);
+      return *this;
+    } else {
+      Node *pParent = pNode->getParent();
+      while(pParent && pParent->m_pChild[0] == pNode) // Mientras venga por el lado izquierdo => subir
+      { pNode = pParent;
+        pParent = pParent->getParent();    }
+      if(pParent && pParent->m_pChild[1] == pNode) // Soy el hijo derecho
+      { setNode(pParent);
+      return *this; }
+     setNode(nullptr);
+    return *this;}
+}
+
+//determina el menor nodo de la izquierda
+template <typename T>
+typename CBinaryTree<T>::Node *CBinaryTree<T>::ElMenor(Node *rParent, size_t c1, size_t c2)
+{
+Node *actual = rParent; 
+
+while (actual->m_pChild[c1]!=nullptr)
+  { 
+    actual=actual->m_pChild[c1];
+  }
+return actual; 
+}
+
+//funcion para borrar nodos
+template <typename T>
+typename CBinaryTree<T>::Node *CBinaryTree<T>::Deleted(Node *rParent, size_t c1, size_t c2,const T &dato){
+  if(rParent == nullptr)
+  {
+    return rParent;
+  }
+  else if (dato < rParent->getData())
+  { 
+    rParent->m_pChild[c1] = Deleted(rParent->m_pChild[c1],c1,c2, dato);
+  }
+
+  else if (dato > rParent->getData())
+  { 
+    rParent->m_pChild[c2] = Deleted(rParent->m_pChild[c2],c1,c2, dato);
+  }
+
+  else
+  {
+    if(rParent->m_pChild[c1] == nullptr)
+    {
+      Node *nuevo = rParent->m_pChild[c2];
+      delete rParent;
+      return nuevo;
+    } 
+    else if(rParent->m_pChild[c2] == nullptr)
+    {
+      Node *nuevo = rParent->m_pChild[c1];
+      delete rParent;
+      return nuevo;
+    } 
+    else 
+    {
+      Node* nuevo = ElMenor(rParent->m_pChild[c2],c1,c2);
+      T valor_modificado = nuevo->getData();
+      rParent->setData(valor_modificado);
+      rParent->m_pChild[c2]=Deleted(rParent->m_pChild[c2],c1,c2, dato);
+    }
+  }
+  return rParent;
+}
+
+#endif 
